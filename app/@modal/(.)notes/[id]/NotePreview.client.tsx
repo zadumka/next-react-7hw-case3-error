b05ab05f3219e05dc51777397e94c758efb56a99
zadumka <90329284+zadumka@users.noreply.github.com>
2025-05-23 15:54:40 +0300

    Update NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,9 +1,7 @@
-"use client";
-
+'use client';
 import { useQuery } from "@tanstack/react-query";
 import Modal from "@/components/Modal/Modal";
 import { fetchNoteById } from "@/lib/api";
-import { useRouter } from "next/navigation";
 
 import css from "./NotePreview.module.css";
 
@@ -12,8 +10,8 @@ interface NotePreviewClientProps {
 }
 
 export default function NotePreviewClient({ id }: NotePreviewClientProps) {
-  const router = useRouter();
-
+ 
+  
   const {
     data: note,
     isLoading,
@@ -23,19 +21,19 @@ export default function NotePreviewClient({ id }: NotePreviewClientProps) {
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
-
+  
   const handleClose = () => {
-    router.back();
+    
+    window.history.back();
   };
-
+  
   if (isLoading) return <p>Loading, please wait...</p>;
   if (error || !note) return <p>Something went wrong.</p>;
-
+  
   return (
     <Modal onClose={handleClose}>
       <div className={css.container}>
         <div className={css.item}>
-          {" "}
           <button className={css.backBtn} onClick={handleClose}>
             Back
           </button>
